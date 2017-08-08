c72220acc94809762fdeebbbc4a277668df25a2e
galvaniccoffee <mail@jannisbecker.de>
2017-08-09 01:17:26 +0200

    Even better BPPianoGenius

--- BPPianoGenius.py
+++ BPPianoGenius.py
@@ -1,5 +1,7 @@
+
+
 class BPPianoGenius:
-    def __init__(keyAmount = 88):
+    def __init__(self, keyAmount = 88):
         #Key Aamount on keyboard
         self.keyAmount = keyAmount
 
@@ -8,11 +10,43 @@ class BPPianoGenius:
         for i in range(0, keyAmount):
             pressedKeys.append(0)
 
+        #This Array contains all keys mapped to one octave
+        self.moduloKeys = []
+        for i in range(0, 12):
+            pressedKeys.append(0)
+
+        #Chordmap: maps tone distances from base key to specific chord
+        self.chordmap = [
+            [0, [0, 4, 7]], #0: Major
+            [1, [0, 3, 7]]  #1: Minor
+        ]
+
+        return
+
+    def refreshModuloKeys():
+        for i in range(0, 12):
+            pressedKeys[i] = 0
+
+        for i in range(0, keyAmount):
+            moduloKeys[i % 12] += pressedKeys[i]
+        return
+
+
 
     def pressKey(index, velocity):
         pressedKeys[index] = velocity;
         
+        return
+        
+
+
     def releaseKey(index):
         pressedKeys[index] = 0;
 
+        return
 
+
+    def detectChord(baseKey):
+        # for i in range(0, len(chordmap)):
+
+        return
