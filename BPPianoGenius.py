9d277db7c09e17560c2bd6f29ae258694fcb1e7b
galvaniccoffee <mail@jannisbecker.de>
2017-08-09 00:54:12 +0200

    Add BPPianoGenius

---
+++ BPPianoGenius.py
@@ -0,0 +1,18 @@
+class BPPianoGenius:
+    def __init__(keyAmount = 88):
+        #Key Aamount on keyboard
+        # self.keyAmount = keyAmount
+
+        # #Intialize Array of pressed keys with zeros
+        # self.pressedKeys = []
+        # for i in range(0, keyAmount):
+        #     pressedKeys.append(
+        #         [0]
+        #         )
+        3
+    def pressKey(index, velocity):
+        #
+        3
+    def releaseKey(index, velocity):
+        #
+        3
